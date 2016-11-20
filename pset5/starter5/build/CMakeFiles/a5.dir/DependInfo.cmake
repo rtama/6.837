8e2455827d9b686ae443b0cc87314f7c680fd790
Reece Tamashiro <reecetamashiro@gmail.com>
2016-11-20 00:53:50 -0500

    Mesh and textures

---
+++ pset5/starter5/build/CMakeFiles/a5.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/3rd_party/lodepng/lodepng.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/3rd_party/lodepng/lodepng.cpp.o"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/src/camera.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/src/camera.cpp.o"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/src/main.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/src/main.cpp.o"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/src/objparser.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/src/objparser.cpp.o"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/src/renderer.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/src/renderer.cpp.o"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/src/starter5_util.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/src/starter5_util.cpp.o"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/src/stb.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/src/stb.cpp.o"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/src/vertexrecorder.cpp" "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/CMakeFiles/a5.dir/src/vertexrecorder.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vecmath/include"
+  "../3rd_party/glfw/include"
+  "../3rd_party/lodepng"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/3rd_party/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/reecetamashiro/code/classes/6.837/pset5/starter5/build/vecmath/CMakeFiles/vecmath.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pset5/starter5/build/CMakeFiles/a5.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a5.dir/3rd_party/lodepng/lodepng.cpp.o"
+  "CMakeFiles/a5.dir/src/main.cpp.o"
+  "CMakeFiles/a5.dir/src/starter5_util.cpp.o"
+  "CMakeFiles/a5.dir/src/camera.cpp.o"
+  "CMakeFiles/a5.dir/src/vertexrecorder.cpp.o"
+  "CMakeFiles/a5.dir/src/objparser.cpp.o"
+  "CMakeFiles/a5.dir/src/stb.cpp.o"
+  "CMakeFiles/a5.dir/src/renderer.cpp.o"
+  "a5.pdb"
+  "a5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
